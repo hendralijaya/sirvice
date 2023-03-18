5c2b2ad26a770d4709c901d7a005806ddade51b0
Patricia Ho <hopatricia1711@gmail.com>
2023-03-18 14:44:46 +0700

    upcoming order + 3 card in home dashboard (still in progress, havent made the responsive)

--- app/views/dashboard/home.php
+++ app/views/dashboard/home.php
@@ -11,6 +11,101 @@
                         <img src="<?= BASEURL; ?>/image/mascot-welcome-crop.png" alt="Mascot Is Welcoming User">
                     </div>
                 </div>
+
+                <div class="main-content">
+                    <div class="upcoming-order">
+                        <div class="upcoming-order-header">
+                            <h5 class="title">Upcoming Order</h5>
+                            <a href="#">View All</a>
+                        </div>
+                        <div class="upcoming-order-cards">
+                            <div class="upcoming-order-card">
+                                <div class="service-technician-status-info">
+                                    <div class="service-technician-info">
+                                        <div class="general-info">
+                                            <p class="service-type">Cleaning Air Conditioner</p>
+                                            <p class="technician-name">Aldy Kusuma</p>
+                                        </div>
+                                        <img src="<?= BASEURL; ?>/image/technician-profile/technician-profile-1.png" alt="Technician Profile Picture">
+                                    </div>
+                                    <div class="status-progress-info">
+                                        <p class="status in-progress">In Progress</p>
+                                    </div>
+                                </div>
+
+                                <div class="date-time-location-info">
+                                    <div class="date-info">
+                                        <i class="material-icons-round">calendar_month</i>
+                                        <time datetime="2023-04-12">Wednesday, 12 April 2023</time>
+                                    </div>
+                                    <div class="time">
+                                        <i class="material-icons-round">access_time</i>
+                                        <time datetime="14:00">14:00 WIB</time>
+                                    </div>
+                                    <div class="location">
+                                        <i class="material-icons-round">location_on</i>
+                                        <address>Jl. Kebon Jeruk Raya No. 1, RT. 1/RW. 1, Kebon Jeruk, Kec. Kby. Baru, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530</address>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="upcoming-order-card">
+                                <div class="service-technician-status-info">
+                                    <div class="service-technician-info">
+                                        <div class="general-info">
+                                            <p class="service-type">Cleaning Air Conditioner</p>
+                                            <p class="technician-name">Aldy Kusuma</p>
+                                        </div>
+                                        <img src="<?= BASEURL; ?>/image/technician-profile/technician-profile-1.png" alt="Technician Profile Picture">
+                                    </div>
+                                    <div class="status-progress-info">
+                                        <p class="status scheduled">Scheduled</p>
+                                    </div>
+                                </div>
+
+                                <div class="date-time-location-info">
+                                    <div class="date-info">
+                                        <i class="material-icons-round">calendar_month</i>
+                                        <time datetime="2023-04-12">Wednesday, 12 April 2023</time>
+                                    </div>
+                                    <div class="time">
+                                        <i class="material-icons-round">access_time</i>
+                                        <time datetime="14:00">14:00 WIB</time>
+                                    </div>
+                                    <div class="location">
+                                        <i class="material-icons-round">location_on</i>
+                                        <address>Jl. Kebon Jeruk Raya No. 1, RT. 1/RW. 1, Kebon Jeruk, Kec. Kby. Baru, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530</address>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <div class="total-summary">
+                        <div class="total-order-card">
+                            <h5 class="title">Total Order</h5>
+                            <p class="total-order-number">12</p>
+                            <i class="material-icons-round">list_alt</i>
+                        </div>
+
+                        <div class="appointment-scheduled-card">
+                            <h5 class="title">Appointment Scheduled</h5>
+                            <p class="appointment-scheduled-number">2</p>
+                            <i class="material-icons-round">access_time</i>
+                        </div>
+
+                        <div class="global-ratings-card">
+                            <h5 class="title">Global Ratings</h5>
+                            <p class="global-ratings-number">2</p>
+                            <i class="material-icons-round">star_border</i>
+                        </div>
+                    </div>
+
+                    <div class="tips-and-tricks">
+
+                    </div>
+                </div>
             </div>
         </section>
         <!-- Section End-->
