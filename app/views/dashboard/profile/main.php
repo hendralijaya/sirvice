dab693a01b88033d8becbe19f578d367d6552b77
Patricia Ho <hopatricia1711@gmail.com>
2023-03-30 23:41:09 +0700

    profile page (two tabs done)

--- app/views/dashboard/profile/main.php
+++ app/views/dashboard/profile/main.php
@@ -1,5 +1,5 @@
         <!-- Section Start -->
-        <section id="order">
+        <section id="profile">
             <div class="container">
                 <div class="page-title order-title-with-button">
                     <h1 class="title-page">Profile</h1>
@@ -16,173 +16,108 @@
 
                     <div id="tab-content">
                         <div class="tab-data tab-data-desc" data-tab="1">
-                            <form action="" class="edit-profile-form">
-                                <div class="profile-image">
-                                    <img src="<?= BASEURL; ?>/image/profile-example.png" alt="User Profile Picture">
-                                    <div class="edit-profile">
-                                        <input type="file">
-                                        <i class="material-icons-round">photo_camera</i>
-                                    </div>
-                                </div>  
+                            <div class="overall-edit-profile">
+                                <form action="" class="edit-profile-form">
+                                    <div class="profile-image">
+                                        <img src="<?= BASEURL; ?>/image/profile-example.png" alt="User Profile Picture">
+                                        <div class="edit-profile">
+                                            <input type="file">
+                                            <i class="material-icons-round">photo_camera</i>
+                                        </div>
+                                    </div>  
 
-                                <div class="input-form">
-                                    <div class="field">
-                                        <label for="name">Name</label>
-                                        <input type="text" id="name" name="name" placeholder="Example. John Doe" required>
-                                    </div>
-                                    
-                                    <div class="field">
-                                        <label for="email">Email</label>
-                                        <input type="email" id="email" name="email" placeholder="Example: hello@gmail.com" required>
+                                    <div class="input-form">
+                                        <div class="field">
+                                            <label for="name">Name</label>
+                                            <input type="text" id="name" name="name" placeholder="Example. John Doe" required>
+                                        </div>
+                                        
+                                        <div class="field">
+                                            <label for="email">Email</label>
+                                            <input type="email" id="email" name="email" placeholder="Example: hello@gmail.com" required>
+                                        </div>
+
+                                        <div class="field">
+                                            <label for="phone_number">Phone Number</label>
+                                            <input type="phone_number" id="phone_number" name="phone_number" placeholder="819xxxxxxxxx" required>
+                                            <span toggle="#phone_number" class="flag-icon"></span>
+                                        </div>
+
+                                        <button type="submit" class="save-changes">Save Changes</button>
                                     </div>
 
-                                    <div class="field">
-                                        <label for="phone_number">Phone Number</label>
-                                        <input type="phone_number" id="phone_number" name="phone_number" placeholder="819xxxxxxxxx" required>
-                                        <span toggle="#phone_number" class="flag-icon"></span>
-                                    </div>
-
-                                    <div class="field password-container">
-                                        <label for="password">Password</label>
-                                        <input type="password" id="password" name="password" placeholder="*******" required>
-                                        <span toggle="#password" class="eye-icon"></span>
+                                </form>
+                            
+                                <div class="attention-tips-from-us">
+                                    <img src="<?= BASEURL; ?>/image/mascot-list-crop.png" alt="Mascot Holding List">
+                                    <div class="attention-tips">
+                                        <h2>Attention</h2>
+                                        <p>You can change your name if you want to, but your email and phone number are locked in. </p>
+                                        <br>
+                                        <p>Don’t forget to save your changes after that!</p>
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="input-form">
-                                    <div class="field">
-                                        <label for="name">Name</label>
-                                        <input type="text" id="name" name="name" placeholder="Example. John Doe" required>
-                                    </div>
-                                    
-                                    <div class="field">
-                                        <label for="email">Email</label>
-                                        <input type="email" id="email" name="email" placeholder="Example: hello@gmail.com" required>
-                                    </div>
-
-                                    <div class="field">
-                                        <label for="phone_number">Phone Number</label>
-                                        <input type="phone_number" id="phone_number" name="phone_number" placeholder="819xxxxxxxxx" required>
-                                        <span toggle="#phone_number" class="flag-icon"></span>
-                                    </div>
-
-                                    <div class="field password-container">
-                                        <label for="password">Password</label>
-                                        <input type="password" id="password" name="password" placeholder="*******" required>
-                                        <span toggle="#password" class="eye-icon"></span>
-                                    </div>
+                            <div class="danger-zone">
+                                <div class="danger-zone-title">
+                                    <h2>Danger Zone</h2>
+                                    <p>Delete user account</p>
                                 </div>
-
-                            </form>
-
+                                <p>By deleting your account, you will lose all your data and access. You can’t recover this account </p>
+                                <a class="delete-account" href="#">Delete My Account</a>
+                            </div>
                         </div>
 
                         <div class="tab-data tab-data-desc" data-tab="2">
-                            <div class="upcoming-order-cards">
-                                <a class="upcoming-order-card" href="#">
-                                    <div class="service-technician-status-info">
-                                        <div class="status-progress-info">
-                                            <div class="status scheduled">
-                                                <i class="material-icons-round">schedule</i>
-                                                <p>Scheduled</p>
-                                            </div>
+                            <div class="overall-change-password">
+                                <form action="" class="change-password-form">
+                                    <div class="input-form">
+                                        <div class="field password-container">
+                                            <label for="password">Current Password</label>
+                                            <input type="password" id="password" name="password" placeholder="*******" required>
+                                            <span toggle="#password" class="eye-icon"></span>
                                         </div>
-                                        <div class="service-technician-info">
-                                            <div class="general-info">
-                                                <p class="service-id-full">Service ID. <span>8195</span></p>
-                                                <p class="technician-name">Aldy Kusuma</p>
-                                            </div>
-                                            <img src="<?= BASEURL; ?>/image/technician-profile/technician-profile-1.png" alt="Technician Profile Picture">
+
+                                        <div class="field password-container">
+                                            <label for="password">New Password</label>
+                                            <input type="password" id="password" name="password" placeholder="*******" required>
+                                            <span toggle="#password" class="eye-icon"></span>
                                         </div>
-                                        <div class="service-types">
-                                            <div class="service-type">
-                                                <i class="material-icons-round">home_repair_service</i>
-                                                <p class="service-type-name">Cleaning AC</p>
-                                            </div>
-                                            <div class="service-type">
-                                                <i class="material-icons-round">home_repair_service</i>
-                                                <p class="service-type-name">Service AC</p>
-                                            </div>
-                                            <div class="service-type">
-                                                <i class="material-icons-round">home_repair_service</i>
-                                                <p class="service-type-name">Repair AC</p>
-                                            </div>
+
+                                        <div class="field password-container">
+                                            <label for="password">Confirm New Password</label>
+                                            <input type="password" id="password" name="password" placeholder="*******" required>
+                                            <span toggle="#password" class="eye-icon"></span>
                                         </div>
+
+                                        <button type="submit" class="save-changes">Save Changes</button>
                                     </div>
 
-                                    <div class="date-time-location-info">
-                                        <div class="date-info">
-                                            <i class="material-icons-round">calendar_month</i>
-                                            <time datetime="2023-04-12">Wednesday, 12 April 2023</time>
-                                        </div>
-                                        <div class="time">
-                                            <i class="material-icons-round">access_time</i>
-                                            <time datetime="14:00">14:00 WIB</time>
-                                        </div>
-                                        <div class="location">
-                                            <i class="material-icons-round">location_on</i>
-                                            <address>Jl. Kebon Jeruk Raya No. 1, RT. 1/RW. 1, Kebon Jeruk, Kec. Kby. Baru, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530</address>
-                                        </div>
-                                    </div>
+                                </form>
 
-                                    <div class="order-date-time">
-                                        <p>Ordered on <time datetime="10-04-2023">Monday, 10 April 2023</time></p>
+                                <div class="attention-tips-from-us">
+                                    <img src="<?= BASEURL; ?>/image/mascot-list-crop.png" alt="Mascot Holding List">
+                                    <div class="attention-tips">
+                                        <h2>Tips from us</h2>
+                                        <p>For security reasons, we recommend changing your password every three months.</p>
+                                        <br>
+                                        <p>It's an easy way to help protect your account and keep your information safe.</p>
                                     </div>
-                                </a>
+                                </div>
                             </div>
                         </div>
 
                         <div class="tab-data tab-data-desc" data-tab="3">
-                            <div class="upcoming-order-cards">
-                                <a class="upcoming-order-card" href="#">
-                                    <div class="service-technician-status-info">
-                                        <div class="status-progress-info">
-                                            <div class="status done">
-                                                <i class="material-icons-round">check</i>
-                                                <p>Done</p>
-                                            </div>
-                                        </div>
-                                        <div class="service-technician-info">
-                                            <div class="general-info">
-                                                <p class="service-id-full">Service ID. <span>8195</span></p>
-                                                <p class="technician-name">Aldy Kusuma</p>
-                                            </div>
-                                            <img src="<?= BASEURL; ?>/image/technician-profile/technician-profile-1.png" alt="Technician Profile Picture">
-                                        </div>
-                                        <div class="service-types">
-                                            <div class="service-type">
-                                                <i class="material-icons-round">home_repair_service</i>
-                                                <p class="service-type-name">Cleaning AC</p>
-                                            </div>
-                                            <div class="service-type">
-                                                <i class="material-icons-round">home_repair_service</i>
-                                                <p class="service-type-name">Service AC</p>
-                                            </div>
-                                            <div class="service-type">
-                                                <i class="material-icons-round">home_repair_service</i>
-                                                <p class="service-type-name">Repair AC</p>
-                                            </div>
-                                        </div>
-                                    </div>
+                            <div class="search-bar-and-add-address">
 
-                                    <div class="date-time-location-info">
-                                        <div class="date-info">
-                                            <i class="material-icons-round">calendar_month</i>
-                                            <time datetime="2023-04-12">Wednesday, 12 April 2023</time>
-                                        </div>
-                                        <div class="time">
-                                            <i class="material-icons-round">access_time</i>
-                                            <time datetime="14:00">14:00 WIB</time>
-                                        </div>
-                                        <div class="location">
-                                            <i class="material-icons-round">location_on</i>
-                                            <address>Jl. Kebon Jeruk Raya No. 1, RT. 1/RW. 1, Kebon Jeruk, Kec. Kby. Baru, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530</address>
-                                        </div>
-                                    </div>
+                            </div>
 
-                                    <div class="order-date-time">
-                                        <p>Ordered on <time datetime="10-04-2023">Monday, 10 April 2023</time></p>
-                                    </div>
+                            <div class="address-cards">
+                                <a class="address-card" href="#">
+                                    <h3 class="title-address">Pradita University</h3>
+                                    <p class="address"></p>
+                                    <p class="note-address"></p>
                                 </a>
                             </div>
                         </div>
