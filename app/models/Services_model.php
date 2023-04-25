84fd6b400697295cbd74f432e41b748c1dab05e4
hendralijaya <hendralijaya@gmail.com>
2023-04-26 00:08:20 +0700

    create feature review

--- app/models/Services_model.php
+++ app/models/Services_model.php
@@ -11,7 +11,7 @@ class Services_model {
 
     public function getAllServices()
     {
-        $this->db->query('SELECT *, TRIM(TRAILING ",000" FROM FORMAT(75000, 3, "id_ID")) AS price FROM ' . $this->table);
+        $this->db->query('SELECT *, TRIM(TRAILING ",000" FROM FORMAT(price, 3, "id_ID")) AS price FROM ' . $this->table);
         return $this->db->resultSet();
     }
 
@@ -36,7 +36,7 @@ class Services_model {
         $order_service = $this->db->resultSet();
         $services = [];
         foreach ($order_service as $os) {
-            $this->db->query('SELECT *, TRIM(TRAILING ",000" FROM FORMAT(75000, 3, "id_ID")) AS price FROM ' . $this->table . ' WHERE id=:id');
+            $this->db->query('SELECT *, TRIM(TRAILING ",000" FROM FORMAT(price, 3, "id_ID")) AS price FROM ' . $this->table . ' WHERE id=:id');
             $this->db->bind('id', $os['service_id']);
             $services[] = $this->db->single();
         }
